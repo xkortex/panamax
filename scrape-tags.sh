f2971d0da69fe71a10a0001b17f69441d73cc765
Michael McDermott <mikemcdermott23@gmail.com>
2020-07-15 09:58:26 -0400

    wip: random tinkering

--- scrape-tags.sh
+++ scrape-tags.sh
@@ -1,8 +1,22 @@
 i=0
 
-while [ $? == 0 ]
-do 
-   i=$((i+1))
-   curl https://registry.hub.docker.com/v2/repositories/nvidia/cuda/tags/\?page\=$i 2>/dev/null|jq '."results"[]["name"]'
+IMAGE=${1:-nvidia/cuda}
+CODE=0
+mkdir -p "$(dirname /tmp/scrape-tags/$IMAGE)"
+while [ "$CODE" == 0 ] && [ "$i" -lt 99 ]; do
+  i=$((i+1))
+  RES=$(curl -sS https://registry.hub.docker.com/v2/repositories/${IMAGE}/tags/\?page\=${i})
+  CODE=$?
+  TAGS=$(echo "$RES" | jq -r '."results"[]["name"]')
+  if [[ $? -ne 0 ]]; then
+    >&2 echo "failed to parse tags. full response:"
+    >&2 echo "$RES"
+    exit 1
+  else
+    echo "$TAGS" | tee -a /tmp/scrape-tags/$IMAGE.txt
+  fi
+  if [[ -z $TAGS ]]; then
+    CODE=-1
+  fi
 
 done
